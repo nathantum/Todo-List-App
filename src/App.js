6a5950a7c743862ec8014fd3f8ce820acb837bd7
nathantum <tumkipsang@gmail.com>
2023-03-06 11:28:03 -0600

    configured tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      
-    </div>
+    <div className="flex h-screen flex-row items-center justify-center gap-x-4 bg-gray text-center">
+    
+  </div>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "react-black": "#282c34",
+        "react-blue": "#61dafb"
+      }
+    },
+  },
+  plugins: [],
+ }
+ 
